65510b81f362f4f9aac637835204844c2f069b1b
Mally13 <mallymutuku13@gmail.com>
2024-05-06 19:07:49 +0300

    Task 1: simple pagination c

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,11 +40,9 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """returns dataset based on index"""
-        assert type(page) == int
-        assert type(page_size) == int
-        assert page > 0
-        assert page_size > 0
-        
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
 
