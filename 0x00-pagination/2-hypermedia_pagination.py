5cebb795993fba18b33fcb197034dfa691f11626
Mally13 <mallymutuku13@gmail.com>
2024-05-07 00:58:11 +0300

    Task 2: more simple pagination c

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+"""
+Contains a function named index_range that takes two integer arguments
+page and page_size
+
+"""
+
+
+def index_range(page, page_size):
+    """
+    Returns a tuple of size two containing start index and
+    end index corresponding to the range of indexes to return
+    in a list for those particular pagination parameters
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns dataset based on index"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+        end_index = min(end_index, len(dataset))
+        return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns dataset and desc based on index"""
+        data = self.get_page(page, page_size)
+        next_page = page + 1 if len(data) == page_size else None
+        prev_page = page - 1 if page > 1 else None
+        total_pages = len(self.dataset()) // page_size + (
+            1 if len(self.dataset()) % page_size != 0 else 0
+            )
+        hypermedia = {
+            "page_size":
+            page_size if len(data) != 0 else 0,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+            }
+        return hypermedia
