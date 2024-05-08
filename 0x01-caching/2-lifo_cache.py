b6955ec20b0cc8fcf7d016424184a98ffc97f219
Mally13 <mallymutuku13@gmail.com>
2024-05-08 16:55:29 +0300

    Task 2: LIFO caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,38 @@
+#!/urs/bin/env python3
+"""
+Contains a class LIFOCache that inherits from BasicCaching
+and is a LIFO caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache defines:
+        A LIFO caching system class
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data_order = []
+
+    def put(self, key, item):
+        """Adds an item to cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                if key in self.cache_data_order:
+                    self.cache_data_order.remove(key)
+                    self.cache_data.pop(key)
+                else:
+                    key_pop = self.cache_data_order.pop(-1)
+                    self.cache_data.pop(key_pop)
+                    print(f'DISCARD: {key_pop}')
+            self.cache_data[key] = item
+            self.cache_data_order.append(key)
+
+    def get(self, key):
+        """Gets the value from the cache based on key"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
